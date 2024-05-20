e1dfd3d12881aedf082e0e7b74814d9347d4c86c
Robin Vandenbroucke <robin.vandenbroucke2@student.vives.be>
2024-05-20 15:23:44 +0200

    teruggeven endpoints beer

--- routes/beers.js
+++ routes/beers.js
@@ -10,3 +10,16 @@ const beers = [
     {id:6, name: 'Guinness', type: 'Brown'},
 ];
 
+//geef alle beers terug
+router.get('/', (req, res) => {
+    res.send(beers);
+});
+
+//geef beers terug met id: ??
+router.get('/:id', (req, res) => {
+    const beer = beers.find(beer => beer.id === parseInt(req.params.id));
+    if (!beer) {
+        res.status(404).send('Beer not found');
+    }
+    res.send(beer);
+})
