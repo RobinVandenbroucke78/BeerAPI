1644dc48874d5790abcb86a21517d42f3edb1901
Robin Vandenbroucke <robin.vandenbroucke2@student.vives.be>
2024-05-20 15:21:28 +0200

    aanmaken beers

---
+++ routes/beers.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const beers = [
+    {id:1, name: 'Stella Artois', type: 'Blond'},
+    {id:2, name: 'Heineken', type: 'Blond'},
+    {id:3, name: 'Leffe', type: 'Blond'},
+    {id:4, name: 'Duvel', type: 'Blond'},
+    {id:5, name: 'Shimay', type: 'Brown'},
+    {id:6, name: 'Guinness', type: 'Brown'},
+];
+
