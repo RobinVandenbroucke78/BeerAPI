4f4046c46866e5f10d7cae082b1d6ba215f882f3
Robin Vandenbroucke <robin.vandenbroucke2@student.vives.be>
2024-05-21 14:20:19 +0200

    aanmaken beer voor db

--- beer.js
+++ beer.js
@@ -7,4 +7,20 @@ mongoose.connect('mongodb+srv://robinvandenbroucke2:MaMTsORgBD24erKY@node.ckpcix
     .then(() => console.log('Connected to DB'))
     .catch(err => console.log("Error connecting to DB: " + err));
 
+const beerSchema = new mongoose.Schema({
+    id: Int32Array,
+    name: String,
+    type: String,
+})
 
+const Beers = mongoose.model('Beers', courseSchema)
+
+
+
+function validateBeers(beer){
+    const schema = Joi.object({
+        name: Joi.string().min(3).required(),
+        type: Joi.string().min(3)
+    })
+    return schema.validate(beer);
+}
