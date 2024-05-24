7e064848b2698e1dc82484a23e84c1a9f3fdea68
Robin Vandenbroucke <robin.vandenbroucke2@student.vives.be>
2024-05-24 18:12:41 +0200

    veranderen middleware naar auth en veranderen van mongoose connection

---
+++ index.js
@@ -0,0 +1,16 @@
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+const mongoose = require('mongoose');
+const beers = require('./routes/beers');
+const express = require('express');
+const app = express();
+
+mongoose.connect('mongodb+srv://robinvandenbroucke2:MaMTsORgBD24erKY@node.ckpcixi.mongodb.net/')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
+app.use(express.json());
+app.use('/api/beer', beers);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
