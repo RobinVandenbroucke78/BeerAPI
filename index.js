d984049d5b0184646507d217b895c31904657a40
Robin Vandenbroucke <robin.vandenbroucke2@student.vives.be>
2024-05-28 14:28:40 +0200

    toevoegen van middleware om een website te tonen

--- index.js
+++ index.js
@@ -4,19 +4,21 @@ const beers = require('./routes/beers');
 const breweries = require('./routes/breweries');
 const users = require('./routes/users');
 const auth = require('./routes/auth');
+const website = require('./middleware/app');
 const express = require('express');
 const app = express();
 const config = require('config');
 
 mongoose.connect('mongodb+srv://robinvandenbroucke2:MaMTsORgBD24erKY@node.ckpcixi.mongodb.net/Eindproject')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...'));
+.then(() => console.log('Connected to MongoDB...'))
+.catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
 app.use('/api/beers', beers);
 app.use('/api/breweries', breweries);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
+app.use('/', website);
 
 /*if (!config.get('jwtPrivateKey')){
   console.error('FATAL ERROR: jwtPrivateKey not defined');
