2e4cd9a3c93e85914d668abe2d43defdac93dc08
Robin Vandenbroucke <robin.vandenbroucke2@student.vives.be>
2024-05-28 13:40:18 +0200

    basic test

---
+++ test/test_beer.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+describe('Basic test', function () {
+  describe('check on beer', function () {
+    it('should equal 15 when 5 is multiplied by 3', function () {
+        var result = 5*3;
+        assert.equal(result, 15);
+    });
+  });
+});
