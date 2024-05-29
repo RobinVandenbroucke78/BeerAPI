3300a85b590418d3e685250f6bce6750dbc4fecc
Robin Vandenbroucke <robin.vandenbroucke2@student.vives.be>
2024-05-29 15:51:12 +0200

    swagger toevoegen aan user

--- routes/users.js
+++ routes/users.js
@@ -2,21 +2,100 @@ const express = require('express');
 const { UserModel, ValidateUser } = require('../models/user');
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-     /* 
-    #swagger.tags = ['Users']
-    #swagger.description = 'Create a new user'
-    #swagger.parameters['body'] = {
-        in: 'body',
-        description: 'User information',
-        required: true,
-        schema: {
-            $username: 'Username',
-            $email: 'example@example.com',
-            $password: 'examplePassword'
-        }
+router.get('/', async (req, res) => {
+    /**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: Get a list of all users
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example:
+ *               data: [{}]
+ *       400:
+ *         description: Bad Request
+ *         content:
+ *          application/json:
+ *            example:
+ *             error:
+ *              message: "Bad Request"
+ */
+    try {
+        const users = await UserModel.find();
+        res.status(302).send(users);
+    } catch (err) {
+        res.status(400).send(err.message);
     }
-    */
+});
+
+router.get('/:id', async (req, res) => {
+    /**
+ * @swagger
+ * /api/users/{id}:
+ *   get:
+ *     summary: Get a user by ID
+ *     tags: [Users]
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: The ID of the user
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example:
+ *               data: [{}]
+ *       404:
+ *         description: User not found
+ */
+    try {
+        const user = await UserModel.findById(req.params.id);
+        if (!user) return res.status(404).send('User not found.');
+        res.status(302).send(user);
+    } catch (err) {
+        res.status(404).send(err.message);
+    }
+});
+
+router.post('/', async (req, res) => {
+    /**
+ * @swagger
+ * /api/users:
+ *   post:
+ *     summary: Create a new user
+ *     tags: [Users]
+ *     requestBody:
+ *       description: user object to be added
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             example:
+ *               data: [{}]
+ *       400:
+ *         description: Invalid request
+ */
     const { error } = ValidateUser(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
@@ -34,50 +113,49 @@ router.post('/', async (req, res) => {
     }  
 });
 
-router.get('/', async (req, res) => {
-     /*
-     #swagger.tags = ['Users']
-     #swagger.description = 'Get all users'
-    */
-    try {
-        const users = await UserModel.find();
-        res.status(302).send(users);
-    } catch (err) {
-        res.status(400).send(err.message);
-    }
-});
-
-router.get('/:id', async (req, res) => {
-     /*
-     #swagger.tags = ['Users']
-     #swagger.description = 'Get a user by ID'
-     #swagger.parameters['id'] = { description: 'User ID', required: true }
-    */
-    try {
-        const user = await UserModel.findById(req.params.id);
-        if (!user) return res.status(404).send('User not found.');
-        res.status(302).send(user);
-    } catch (err) {
-        res.status(404).send(err.message);
-    }
-});
-
 router.patch('/:id', async (req, res) => {
-     /*
-     #swagger.tags = ['Users']
-     #swagger.description = 'Partially update a user by ID'
-     #swagger.parameters['id'] = { description: 'User ID', required: true }
-     #swagger.parameters['body'] = {
-         in: 'body',
-         description: 'Fields to update',
-         required: true,
-         schema: {
-             name: 'UpdatedUsername',
-             email: 'updated@example.com',
-             password: 'updatedPassword'
-         }
-     }
-    */
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   patch:
+ *     summary: Update a user by ID
+ *     description: Update the details of a user by providing the user ID.
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         description: The ID of the user to be updated.
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       description: Updated user information
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Successful update
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: 'User updated successfully'
+ *       404:
+ *         description: User not found
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: 'User not found'
+ */  
     const userId = req.params.id;
     const updateFields = req.body;
 
@@ -99,21 +177,48 @@ router.patch('/:id', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-     /*
-     #swagger.tags = ['Users']
-     #swagger.description = 'Fully update a user by ID'
-     #swagger.parameters['id'] = { description: 'User ID', required: true }
-     #swagger.parameters['body'] = {
-         in: 'body',
-         description: 'User information',
-         required: true,
-         schema: {
-             $name: 'UpdatedUsername',
-             $email: 'updated@example.com',
-             $password: 'updatedPassword'
-         }
-     }
-    */
+    /**
+ * @swagger
+ * /api/users/{id}:
+ *   put:
+ *     summary: Update a user by ID
+ *     description: Update the details of a user by providing the user ID.
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         description: The ID of the User to be updated.
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       description: Updated User information
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Successful update
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: 'User updated successfully'
+ *       404:
+ *         description: User not found
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: 'User not found'
+ */
     const { error } = ValidateUser(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
@@ -138,11 +243,25 @@ router.put('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-     /*
-     #swagger.tags = ['Users']
-     #swagger.description = 'Delete a user by ID'
-     #swagger.parameters['id'] = { description: 'User ID', required: true }
-    */
+    /**
+ * @swagger
+ * /api/users/{id}:
+ *   delete:
+ *     summary: Delete a user by ID
+ *     tags: [Users]
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: The ID of the user
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       204:
+ *         description: User deleted successfully
+ *       404:
+ *         description: User not found
+ */
     try {
         const user = await UserModel.findByIdAndDelete(req.params.id);
         if (!user) return res.status(404).send('User not found.');
