18c088bc59a05102dcfcb197fee6a764c06dfbdb
Robin Vandenbroucke <robin.vandenbroucke2@student.vives.be>
2024-06-04 09:03:06 +0200

    aanpassen auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,36 +1,33 @@
-const Joi = require('joi');
-const bcrypt = require('bcrypt');
-const {User} = require('../models/user');
-const mongoose = require('mongoose');
-const _ = require('lodash');
+const { User } = require('../models/user');
 const express = require('express');
 const router = express.Router();
+const Joi = require('joi');
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
-
 router.post('/', async (req, res) => {
-    const { error } = validate(req.body); 
+    const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
-  
-    let user = await User.findOne({email: req.body.email});
-    if (!user) return res.status(400).send('Invalid user or password');
 
-    const validPassword = await bcrypt.compare(req.body.password, user.password)
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) res.status(400).send('Invalid email or password');
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
     if(!validPassword) return res.status(400).send('Invalid email or password');
-
+    
     const token = user.generateAuthToken();
-
-    res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']))
-
+    res.send(token);
 });
 
-function validate(req){
+function validate(req) {
     const schema = Joi.object({
-        email: Joi.string().min(5).max(255).email().required(),
-        password: Joi.string().min(5).max(1024).required(),
+        email: Joi.string().min(5).max(50).email().required(),
+        password: Joi.string().min(5).max(255).required()
     });
     return schema.validate(req);
 }
+
 
 module.exports = router;
